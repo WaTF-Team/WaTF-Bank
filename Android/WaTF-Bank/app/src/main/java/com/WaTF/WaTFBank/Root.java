7b46ea169130dbf9286da9a3f08c47e2eeec67ad
boonpoj <boonpoj@gmail.com>
2019-01-07 17:51:37 +0700

    update Android Broadcast

--- Android/WaTF-Bank/app/src/main/java/com/WaTF/WaTFBank/Root.java
+++ Android/WaTF-Bank/app/src/main/java/com/WaTF/WaTFBank/Root.java
@@ -1,7 +1,7 @@
 package com.WaTF.WaTFBank;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+        import android.support.v7.app.AppCompatActivity;
+        import android.os.Bundle;
 
 public class Root extends AppCompatActivity {
 
